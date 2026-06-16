535abb62e17b4e2b4de28dd92a80daed7a11eccd
Aadeesh Sharma <sharmaaadeesh@gmail.com>
2026-06-16 13:37:56 +0100

    improved formatting

--- src/services/dentist_matching.py
+++ src/services/dentist_matching.py
@@ -1,3 +1,4 @@
+import numpy as np
 from typing import List
 from src.utils.similarity_utils import calculate_cosine_similarity
 from typing import Dict
@@ -8,14 +9,19 @@ from src.utils.tfidf_utils import tfidf_utils
 async def dentist_matcher(
     dental_preferences: List[str], tfidf_instance: tfidf_utils
 ) -> Dict[str, spmatrix]:
+
+    # Extracting our vectorized dentist data and ids from the global instance
     dentist_vectors = tfidf_instance.corpus_vectors
     dentist_ids = tfidf_instance.corpus_ids
+
+    # Converting users dental prefrences to a numpy array and then vectorizing it
     dental_preferences = [" ".join(dental_preferences)]
     dental_prefs_vector = tfidf_instance.transform_document(dental_preferences)
 
+    # Calculating cosine similarities which returns a 2d array of size (1, len(dentist_vectors))
     similarities = calculate_cosine_similarity(dental_prefs_vector, dentist_vectors)
 
-    # Combine dentist IDs with their similarity scores and sort
+    # Combine dentist IDs with their similarity scores
     results = zip(dentist_ids, similarities[0])
 
     return {dentist_id: similarity_score for dentist_id, similarity_score in results}
--- src/utils/tfidf_utils.py
+++ src/utils/tfidf_utils.py
@@ -26,10 +26,8 @@ class tfidf_utils:
         print(f"Vectorizer fitted with a corpus of {len(self.corpus_ids)} documents.")
         return self.corpus_vectors
 
-    def transform_document(self, document: List[str]):
+    def transform_document(self, document: List[str]) -> csr_matrix:
         if not self.vectorizer:
             raise ValueError("Vectorizer is not fitted. Run fit_vectorizer first.")
 
         return self.vectorizer.transform(document)
-
-    # def initialize(self):
